f13fc30557b69da1422504b8efd06015ceb8784b
vinay <vinay8888@gmail.com>
2021-11-08 00:00:58 +0100

    cleanup

---
+++ tutor.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+echo "Printing text with newline"
+echo -n "Printing text without newline"
+echo -e "\nRemoving \t backslash \t characters\n"
+
+# Add two numeric value
+
+((sum=25+35))
+
+#Print the result
+echo $sum
